c0602bbafd358a216058d6726bc2e23b0c95cb5a
girish <girishkoni017@gmail.com>
2026-04-16 22:16:41 +0530

    new

--- backend/services/stt_service.py
+++ backend/services/stt_service.py
@@ -3,7 +3,7 @@ import wave
 import io
 import numpy as np
 
-COLAB_WHISPER_URL = "https://soldier-paying-struct-familiar.trycloudflare.com/transcribe"
+COLAB_WHISPER_URL = "https://way-attempting-pushing-carrier.trycloudflare.com/transcribe"
 
 class STTService:
     def __init__(self, url: str = COLAB_WHISPER_URL):
