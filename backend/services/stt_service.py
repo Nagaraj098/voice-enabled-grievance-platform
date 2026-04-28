f88efe95d0294f13fd1ee012abb42716874662ba
Nagaraj hosamani <nagarajyhosamani2004@gmail.com>
2026-04-28 16:31:35 +0530

    fixed view file in backend and updated code to stop audio

--- backend/services/stt_service.py
+++ backend/services/stt_service.py
@@ -3,7 +3,7 @@ import wave
 import io
 import numpy as np
 
-COLAB_WHISPER_URL = "https://garbage-trend-tower-prophet.trycloudflare.com/transcribe"
+COLAB_WHISPER_URL = "https://microwave-methodology-reliable-boxed.trycloudflare.com/transcribe"
 
 class STTService:
     def __init__(self, url: str = COLAB_WHISPER_URL):
