cae0c8f59dbf94a6e8e6ea33108ab40bbf97cd7f
Nagaraj hosamani <nagarajyhosamani2004@gmail.com>
2026-04-29 09:11:35 +0530

    commit before pull

--- test_stt.py
+++ test_stt.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://garbage-trend-tower-prophet.trycloudflare.com/transcribe"
+url = "https://hills-lynn-olive-cheapest.trycloudflare.com/transcribe"
 
 with open(r"D:\Internship\grievance project\Voice_enabled_grievance_platform\test.wav", "rb") as f:
     response = requests.post(url, files={"audio": f})
