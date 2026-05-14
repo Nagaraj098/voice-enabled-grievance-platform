5082174e7aea02bdd3d97fca1b65db14bb3a8527
Nagaraj hosamani <nagarajyhosamani2004@gmail.com>
2026-05-14 08:10:24 +0530

    improved latecncy

--- test_stt.py
+++ test_stt.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://logo-judicial-ideas-oral.trycloudflare.com/transcribe"
+url = "https://penalties-flood-purchase-engage.trycloudflare.com/transcribe"
 
 with open(r"D:\Internship\grievance project\Voice_enabled_grievance_platform\test.wav", "rb") as f:
     response = requests.post(url, files={"audio": f})
